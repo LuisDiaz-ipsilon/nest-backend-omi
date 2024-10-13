ae443ddfcb6a07547a8d43b7495a4732bfbf2e76
LuisDiaz-ipsilon <54384617+LuisDiaz-ipsilon@users.noreply.github.com>
2024-10-13 17:48:48 -0600

    other wise

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    })
-  );
+
 
   app.enableCors({
-    origin: ['http://localhost:4200', 'http://134.255.176.206', 'https://omi-murex.vercel.app'], // Allowed origins
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
-    credentials: true, // Allow credentials (e.g., cookies)
-    allowedHeaders: 'Content-Type, Accept', // Allowed headers
+    origin: [
+      'http://localhost:4200', 'http://134.255.176.206', 'https://omi-murex.vercel.app'
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
   });
-
 
   await app.listen(3000);
 }
